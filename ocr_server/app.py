b822d012565d445d44bbcb6c5806ae211bbe0ccb
simran045 <ssimran20agt@gmail.com>
2021-04-18 21:50:31 +0530

    Adding all files

---
+++ ocr_server/app.py
@@ -0,0 +1,63 @@
+import os
+
+from flask import Flask, render_template, request,redirect
+
+from ocr_core import ocr_core
+
+from os import path,walk
+
+
+UPLOAD_FOLDER = '/static/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+
+extra_dirs = ['directory/to/watch',]
+extra_files = extra_dirs[:]
+for extra_dir in extra_dirs:
+    for dirname, dirs, files in walk(extra_dir):
+        for filename in files:
+            filename = path.join(dirname, filename)
+            if path.isfile(filename):
+                extra_files.append(filename)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def home_page():
+    return redirect("/upload",code=302)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return render_template('upload.html', msg='No file selected')
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return render_template('upload.html', msg='No file selected')
+
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
+
+            # call the OCR function on it
+            extracted_text = ocr_core(file)
+
+            # extract the text and display it
+            return render_template('upload.html',
+                                   msg='Successfully processed',
+                                   extracted_text=extracted_text,
+                                   img_src=UPLOAD_FOLDER + file.filename)
+    elif request.method == 'GET':
+        return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(extra_files=extra_files)
